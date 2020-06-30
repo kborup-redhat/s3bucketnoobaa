3d1d3e7a8dec3811c3a5e34e23e604820af8ca33
Kim Borup <kborup@redhat.com>
2020-06-30 11:37:06 +0200

    test

--- config.php
+++ config.php
@@ -14,21 +14,21 @@ $c['base-path'] = getenv_default('BASE_PATH');
 $c['theme'] = getenv_default('THEME', 'plain');
 
 // Text to use as page header
-$c['page-header'] = getenv_default('PAGE_HEADER', 'VPSEC Nightwatch reports');
+$c['page-header'] = getenv_default('PAGE_HEADER', 'VPSECNightwatchreports');
 
 // File size in bytes over which to serve files as torrents
 $c['torrent-threshold'] = getenv_default('TORRENT_THRESHOLD', null);
 
 // Amazon S3 access information
-$c['s3-access-key'] = Ttu5QcGq78aLBK7jvNxv;
-$c['s3-secret-key'] = IEDDGW4eNYJq7jKe6OK5ci1nSa8xYNQtQvALeppi;
+$c['s3-access-key'] = getenv_default('S3_ACCESS_KEY' , 'Ttu5QcGq78aLBK7jvNxv');
+$c['s3-secret-key'] = getend_default(i'S3_SECRET_KEY' , 'IEDDGW4eNYJq7jKe6OK5ci1nSa8xYNQtQvALeppi');
 
 // Bucket information should be cached so your S3 account doesn't need to be queried for every user request. Default cache-time is 10 minutes.
 $c['cache-time'] = getenv_default('CACHE_TIME', 60 * 10);
 $c['cache-dir'] = getenv_default('CACHE_DIR', '/tmp');
 
 // Bucket
-$c['bucket-name'] = web-bucket-4cf101e4-5ca2-400b-ae1e-5345a1b4a747;
+$c['bucket-name'] = getenv_default('BUCKET_NAME' , 'web-bucket-4cf101e4-5ca2-400b-ae1e-5345a1b4a747');
 $c['bucket-url-prefix'] = getenv_default('BUCKET_URL_PREFIX', 'https://s3-openshift-storage.apps.vpsec.emea-1.rht-labs.com'/$c['bucket-name']);
 
 // Google analytics id to use for tracking
